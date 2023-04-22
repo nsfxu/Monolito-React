b04e680cfc35c46fc9044afa4bbcefac72cf54c0
Giusier <giusier@hotmail.com>
2023-04-22 19:07:31 -0300

    even better base

--- src/components/TabSwinlaneGroup/TabSwinlaneGroup.js
+++ src/components/TabSwinlaneGroup/TabSwinlaneGroup.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-import { Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-// import TabColumn from '../TabColumn';
+import TabColumn from '../TabColumn';
 
 /* eslint-disable */
 // eslint-disable-next-line
-const TabSwinlaneGroup = ({
-    all_columns,
-    index,
-    provided,
-    snapshot
-}) => {
+const TabSwinlaneGroup = ({ all_columns, index, provided, snapshot }) => {
     const grid = 8;
+
+    const getItemGroupStyle = (isDragging, draggableStyle) => ({
+        // some basic styles to make the items look a bit nicer
+        userSelect: 'none',
+        padding: grid * 2,
+        margin: `0 ${grid}px 0 0`,
+
+        // change background colour if dragging
+        background: isDragging ? 'lightgreen' : 'grey',
+
+        // styles we need to apply on draggables
+        ...draggableStyle
+    });
 
     const getItemStyle = (isDragging, draggableStyle) => ({
         // some basic styles to make the items look a bit nicer
@@ -29,22 +37,29 @@ const TabSwinlaneGroup = ({
     });
 
     const getListStyle = (isDraggingOver) => ({
-        background: isDraggingOver ? 'lightgreen' : 'grey',
+        background: isDraggingOver ? 'lightblue' : 'lightgrey',
 
         display: 'flex',
         padding: grid,
         overflow: 'auto'
     });
 
+    const onDragEnd = (result) => {
+        console.log(result);
+    };
+
     return (
-        
-        <Draggable key={`swinlane-group`} draggableId={`swinlane-group`} index={index}>
+        <Draggable
+            key={`swinlane-group`}
+            draggableId={`swinlane-group`}
+            index={index}
+        >
             {(provided, snapshot) => (
                 <div
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    style={getItemStyle(
+                    style={getItemGroupStyle(
                         snapshot.isDragging,
                         provided.draggableProps.style
                     )}
@@ -52,8 +67,53 @@ const TabSwinlaneGroup = ({
                     //     setSelectedColumn(`${column.id}`);
                     // }}
                 >
-                    {/* <TabColumn column_info={column} /> */}
-                    <h1>Swinlane Group</h1>
+                    <DragDropContext onDragEnd={onDragEnd}>
+                        <Droppable
+                            droppableId="rowSwinlaneGroup"
+                            direction="horizontal"
+                        >
+                            {(provided, snapshot) => (
+                                <div
+                                    ref={provided.innerRef}
+                                    style={getListStyle(
+                                        snapshot.isDraggingOver
+                                    )}
+                                    {...provided.droppableProps}
+                                >
+                                    {all_columns.map((column, index) => (
+                                        <Draggable
+                                            key={`${column.id}`}
+                                            draggableId={`${column.id}`}
+                                            index={index}
+                                        >
+                                            {(provided, snapshot) => (
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    style={getItemStyle(
+                                                        snapshot.isDragging,
+                                                        provided.draggableProps
+                                                            .style
+                                                    )}
+                                                    // onClick={() => {
+                                                    //     setSelectedColumn(
+                                                    //         `${column.id}`
+                                                    //     );
+                                                    // }}
+                                                >
+                                                    <TabColumn
+                                                        column_info={column}
+                                                    />
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                 </div>
             )}
         </Draggable>
