271c84b60b5d2f88ea87e666e0afbbea4da984c0
Giusier <giusier@hotmail.com>
2023-06-20 21:15:42 -0300

    base wip front end done

--- src/components/ColumnHeader/ColumnHeader.js
+++ src/components/ColumnHeader/ColumnHeader.js
@@ -1,16 +1,47 @@
 import React, { useState, useEffect } from 'react';
-
+/* eslint-disable */
+// eslint-disable-next-line
 import propTypes from 'prop-types';
 
 import { Button, Stack, Typography } from '@mui/material';
 
 const titleColor = 'white';
-/* eslint-disable */
-// eslint-disable-next-line
+const normalWip = 'white';
+const warningWip = 'yellow';
+const dangerWip = 'red';
+
 const ColumnHeader = ({ this_column, openModal }) => {
     const [cardCount, setCardCount] = useState(0);
+    const [wipColor, setWipColor] = useState(normalWip);
 
-    useEffect(() => {}, [this_column]);
+    useEffect(async () => {
+        if (!this_column.showWip) {
+            return;
+        }
+
+        let count = 0;
+
+        if (this_column.groups.length > 0) {
+            this_column.groups.map((groups) => (count += groups.cards.length));
+        }
+
+        await setCardCount(count);
+    }, [this_column]);
+
+    useEffect(async () => {
+        if (!this_column.showWip) {
+            return;
+        }
+        const result = this_column.wip_limit - cardCount;
+
+        if (result <= 0) {
+            await setWipColor(dangerWip);
+        } else if (result > 0 && result <= 2) {
+            await setWipColor(warningWip);
+        } else {
+            await setWipColor(normalWip);
+        }
+    }, [cardCount]);
 
     console.log(this_column);
     return (
@@ -31,7 +62,7 @@ const ColumnHeader = ({ this_column, openModal }) => {
                 </div>
                 {this_column.showWip && (
                     <div>
-                        <Typography variant="h6">
+                        <Typography variant="h6" sx={{ color: wipColor }}>
                             {cardCount}/{this_column.wip_limit}
                         </Typography>
                     </div>
